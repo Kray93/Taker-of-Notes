174f0a8b8a07ef6a0dabbeb653a2c5f9d7ab630d
Kray93 <kevinwillig@gmail.com>
2021-01-13 22:17:08 -0800

    file structure, finished server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+let PORT = 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+// HTML route
+const htmlRoute = require("./route/htmlRoute");
+app.use(htmlRoute);
+
+// API route
+const apiRoute = require("./route/apiRoute");
+app.use(apiRoute);
+
+// server listen
+app.listen(PORT, ()=>{
+    console.log(`PORT ${PORT} listening!`);
+});
